9283f5d9d1965bb590cc6043702e6446bcca2781
JWBlackHole <blackhole.ljw@gmail.com>
2024-03-06 18:07:56 +0800

    my picture animation

--- src/components/intro/MyPictureAndName.js
+++ src/components/intro/MyPictureAndName.js
@@ -3,11 +3,14 @@
 import Image from 'react-bootstrap/Image';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './MyPictureAndName.css';
 
 function MyPictureAndName() {
   return (
-    <div className='d-flex flex-column align-items-center p-3'>
-        <Image src={require("../../imgs/selfie.jpeg")} alt='me' width={150} roundedCircle />
+    <div className='d-flex flex-column align-items-center p-3 imgdiv'>
+        <div className='imgdiv rounded-circle' style={{overflow: 'hidden', width: '150px'}}>
+          <Image src={require("../../imgs/selfie.jpeg")} alt='me' width={150} roundedCircle />
+        </div>
         <div className='mt-3'>
           <div className='d-flex justify-content-center'>
               <p className='h5' style={{color:'#cccccc', fontFamily: 'Roboto, serif', fontWeight: "bold" }}>JWBlackHole</p>
