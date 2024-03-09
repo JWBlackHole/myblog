95c18b6e0a841c81be4b9b2d0d3a0e2a91801a41
william0208 <williamlin0208@gmail.com>
2024-03-10 01:52:45 +0800

    Layout

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -6,7 +6,7 @@ import './Category.css';
 function Category() {
 
   return (
-    <div className='h-100 d-flex flex-column justify-content-center'>
+    <div className='h-100 d-flex flex-column justify-content-lg-center'>
       <div className="d-flex flex-column my-4 mx-3 mx-md-4" style={{position: 'relative', backgroundColor: 'rgba(52, 52, 52, 0.8)', minHeight: '50%'}}>
         <div className="d-flex flex-column flex-lg-row align-items-lg-center" style={{borderLeft: '5px #999999 solid', padding: '2rem 2rem 1rem 5%'}}>
           {/* <div className="flex-grow-1"><a href={`#/article?postid=${index}`}><h2 style={{color: "#cfcfcf", margin: 0}}>ahha</h2></a></div> */}
